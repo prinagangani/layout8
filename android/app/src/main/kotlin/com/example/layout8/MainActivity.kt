ca3a26fde253e116f1ee5fc5e12ebfca5497880f
country-list <prinagangani10@gmail.com>
2023-02-13 16:02:19 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/layout8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.layout8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/eight.dart
@@ -0,0 +1,280 @@
+import 'package:flutter/material.dart';
+
+class eight extends StatefulWidget {
+  const eight({Key? key}) : super(key: key);
+
+  @override
+  State<eight> createState() => _eightState();
+}
+
+class _eightState extends State<eight> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "Profile",
+                          style: TextStyle(
+                              color: Colors.white,
+                              letterSpacing: 1,
+                              fontSize: 18),
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                            width: 10,
+                          ),
+                        ),
+                        Icon(Icons.search, color: Colors.white),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(
+                          Icons.more_vert_sharp,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Pawan Kumar",
+                        style: TextStyle(
+                            color: Colors.white,
+                            letterSpacing: 1,
+                            fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("Flutter Developer",
+                          style: TextStyle(
+                              fontSize: 12,
+                              letterSpacing: 1,
+                              color: Colors.white)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "1.5K",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text("Posts",
+                          style: TextStyle(color: Colors.black, fontSize: 12)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "2.5K",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text("Followers",
+                          style: TextStyle(color: Colors.black, fontSize: 12)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "10K",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text("Comments",
+                          style: TextStyle(color: Colors.black, fontSize: 12)),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "1.2K",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text("Following",
+                          style: TextStyle(color: Colors.black, fontSize: 12)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 60),
+                    child: Text(
+                      "Photos",
+                      style: TextStyle(
+                          color: Colors.black,
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 60, right: 60),
+                  child: Container(
+                    height: 80,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        FlutterLogo(size: 50),
+                        FlutterLogo(size: 50),
+                        FlutterLogo(size: 50),
+                        FlutterLogo(size: 50),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 60),
+                    child: Text(
+                      "Post",
+                      style: TextStyle(
+                          color: Colors.black,
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 60,right: 60),
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.blue, shape: BoxShape.circle),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Pawan Kumar posted a photo",style: TextStyle(fontSize: 12),),
+                        Text("25 mins ago",style: TextStyle(fontSize: 12),),
+                      ],
+                    ),
+
+                  ],
+                ),
+
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Container(
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.brown,
+                  borderRadius: BorderRadius.circular(2),
+                ),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.grey.shade100,
+      ),
+    );
+  }
+}
